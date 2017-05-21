218e29a95838426715dfb03b9eb4afd994188be8
Quang Huy <huy.tranquang116@gmail.com>
2017-05-21 18:50:30 +0700

    gui bai tap anh tien

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string user_name = "Tran Quang Huy";
+    cout << user_name<< endl;
+    cin.get();
+    return 0;
+}
